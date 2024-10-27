17ea752e186429cf759e3bca277b33bb4e653025
Chen Jun-Shi <cjuns@ustc.edu.cn>
2024-10-27 21:23:48 +0800

    fix: run dummysize with bsub only on linuxSW64

--- etc/makeFiles/scotch/bsub.sh
+++ etc/makeFiles/scotch/bsub.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-bsub -I -b -share_size 14000 -cross_size 128 -cache_size 32 -q q_sw_expr -n 1 -cgsp 64 "$@"
+if [ "$WM_ARCH" = linuxSW64 ]; then
+  bsub -I -b -share_size 14000 -cross_size 128 -cache_size 32 -q q_sw_expr -n 1 -cgsp 64 "$@"
+else
+  "$@"
+fi
